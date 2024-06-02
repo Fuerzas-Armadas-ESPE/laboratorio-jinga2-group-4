518a24cae592782f4e15bc945ae6f632b9942a0b
Edgar <ejgallegos@espe.edu.ec>
2024-06-01 20:56:27 -0500

    chore: Remove unused files and code

--- productos/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # URLs de vistas normales
-    # Crear la URL de la vista index
-]
--- productos/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Producto
-
-productos = []
-
-def listar_productos(request):
-    # Consulta a la base de datos
-    # Renderiza la plantilla listar.html
